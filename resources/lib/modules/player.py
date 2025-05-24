f0eec34ea6a3be7303e90848f382661982340d33
frankdpGH <frank.depelsmaeker@gmail.com>
2025-05-24 16:59:10 +0200

    Add TVGuide and IPTVManager integration

--- resources/lib/modules/player.py
+++ resources/lib/modules/player.py
@@ -32,6 +32,15 @@ class Player:
         # if broadcast and broadcast.video_url:
         #     self.play_from_page(broadcast.video_url)
         #     return
+        if "lay" in uuid:  #Channel name from IPTV_manager => get uuid
+            try:
+                items = self._api.get_live_channels()
+            except Exception as ex:
+                kodiutils.notification(message=str(ex))
+                raise
+            channel = next(channel for channel in items if channel.title == uuid)
+            print(f"uuid entry is {uuid}")
+            uuid=channel.uuid
 
         self.play(uuid, 'live_channel')
 
